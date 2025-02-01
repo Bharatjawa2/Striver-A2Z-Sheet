2b0704f5456ff478c2c39d3f9d99b76574975ebd
Bharat <jawabharat05@gmail.com>
2025-02-01 09:49:09 +0530

    BuyStock1

---
+++ DP/BuyAndSellStock1/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void main(String[] args) {
+        int[] prices={7,1,5,3,6,4};
+        int ans=maxProfit(prices);
+        System.out.println(ans);
+    }
+    public static int maxProfit(int[] prices) {
+        int profit=0;
+        int min=prices[0];
+        for(int i=1;i<prices.length;i++){
+            int cost=prices[i]-min;
+            profit=Math.max(profit,cost);
+            min=Math.min(min,prices[i]);
+        }
+        return profit;
+    }
+}
